28c47abddc75d56fa18f7dccfe60eb78e51c5870
lai0630 <11056024@ntub.edu.tw>
2023-10-30 00:52:14 +0800

    錯的

--- mysite/views.py
+++ mysite/views.py
@@ -11,11 +11,9 @@ def homepage(request):
     return render(request, 'index.html', locals())
 
 def showpost(request,slug):#request浏览器向服务器发送的请求对象，包含用户信息、请求内容和请求方式等
-    try:
-        post = Post.objects.get(slug=slug)#是從數據庫取得一個匹配的結果 返回一個對象 如果紀錄不存在的話 會回復錯誤
-        if post!= None:
-            return render(request,'post.html',locals())#前兩個是必要的
-        else:
-            return redirect("/")
-    except:
-        return redirect("/")#redirect轉網址
+    post = Post.objects.get(slug=slug)#是從數據庫取得一個匹配的結果 返回一個對象 如果紀錄不存在的話 會回復錯誤
+    if post!= None:
+        return render(request,'post.html',locals())#前兩個是必要的
+    else:
+        return redirect("/")
+    return redirect("/")#redirect轉網址
